fd36de4c334250df195324e95e6f85931d5cbcb6
vijaikodi <73403829+vijaikodi@users.noreply.github.com>
2025-03-18 12:06:31 +0100

    iteration2

--- firefox/tools/chocolateyInstall.ps1
+++ firefox/tools/chocolateyInstall.ps1
@@ -20,7 +20,7 @@ if (Get-32bitOnlyInstalled -product $softwareName) {
 }
 
 $sa = ""
-
+#test
 # Command Line Options from the Firefox installer
 # https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 
